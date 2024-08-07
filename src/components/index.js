456c571946e6d1cfe7f557eb022cb77c21bf3e94
Aditya <aditya.vishwabharati@gmail.com>
2024-08-07 20:36:29 +0530

    updated logo import

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,13 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
-import logo from "./Logo"
+import Logo from "./Logo"
 import LogoutBtn from "./header/LogoutBtn";
 
 export { 
     Header, 
     Footer,
     Container,
-    logo,
+    Logo,
     LogoutBtn
  };
