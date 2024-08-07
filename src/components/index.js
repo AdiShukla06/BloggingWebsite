cd3845412d2045a6e7b62ee3b9559b15de893128
Aditya <aditya.vishwabharati@gmail.com>
2024-08-07 14:54:20 +0530

    exporting all the component from index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,13 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-
+import Container from "./container/Container";
+import logo from "./Logo"
+import LogoutBtn from "./header/LogoutBtn";
 
 export { 
     Header, 
-    Footer
+    Footer,
+    Container,
+    logo,
+    LogoutBtn
  };
