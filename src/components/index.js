8fd3a45fbc558cd62ba449e0567ce96b101e17d2
Aditya <aditya.vishwabharati@gmail.com>
2024-08-07 23:59:09 +0530

    imported some more component here

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo"
 import LogoutBtn from "./header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
 
 export { 
     Header, 
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button
  };
